e380903766d325d11b1fd4d12d5d0fc116a5273a
Kajal Rajak <rajakkajal219@gmail.com>
2025-12-16 18:26:31 +0530

    staring new project in React

---
+++ first_project/src/componets/MainHeader/Mainheader.jsx
@@ -0,0 +1,14 @@
+const MainHeader = () =>{
+    return(
+        <header>
+            <div class="main_header_logo">
+                </div >
+                <nav class="main_header_navlinks">
+
+                </nav>
+                <div class="main_header_button">
+            </div>
+        </header>
+    )
+}
+export default  MainHeader;
---
+++ first_project/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+const Home = () =>{
+    return(
+        <>
+        This is home where components will go
+        </>
+    )
+}
+export default Home;
